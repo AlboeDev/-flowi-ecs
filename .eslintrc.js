91ff3afb479f7d2ed3e93696f8ecfeda8ffca41c
Timothy Scheuering <interactivetimmy@gmail.com>
2021-12-16 20:05:59 -0500

    feat(config/eslint): generate core configuration

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-tsdoc',
+    'jest',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'tsdoc/syntax': 'warn',
+  },
+};
