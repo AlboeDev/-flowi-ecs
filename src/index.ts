c199cebfe4b6e0056cd3ec8662c59e10b6e3d69a
Timothy Scheuering <interactivetimmy@gmail.com>
2021-12-16 19:27:33 -0500

    docs: add template documentation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Return the provided value.
+ *
+ * @public
+ * @param value - Value to return.
+ * @returns - The value provided.
+ */
 const func = (value: string): string => {
   return value;
 };
